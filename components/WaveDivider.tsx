7e0731859590f2cdc77d4c90eeb18031c8e199e6
Mian Hassam <kbpro667@gmail.com>
2025-11-08 12:45:36 +0500

    Updates

--- components/WaveDivider.tsx
+++
@@ -1,9 +0,0 @@
-export default function WaveDivider(){
-  return (
-    <div className="-mt-6">
-      <svg viewBox="0 0 1440 60" className="w-full h-12" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 30 C 360 0 1080 60 1440 30 L1440 60 L0 60 Z" fill="rgba(255,255,255,0.6)" />
-      </svg>
-    </div>
-  )
-}
