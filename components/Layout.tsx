c60bc0a00c71cf9abac1a98a95da93b21897e93f
Mian Hassam <mianhassam96@gmail.com>
2026-02-16 09:02:42 +0500

    feat: Add futuristic interactive navbar with advanced animations and dropdowns

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import Header from './Header'
+import FuturisticNav from './FuturisticNav'
 import Footer from './Footer'
 import WhatsAppPopup from './WhatsAppPopup'
 import WhatsAppButton from './WhatsAppButton'
 import ConsultationPopup from './ConsultationPopup'
 
 const Layout: React.FC<{children: React.ReactNode}> = ({ children }) => {
+  const router = useRouter()
+  
+  // Use futuristic nav for homepage, regular header for other pages
+  const useFuturisticNav = router.pathname === '/'
+
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
+      {useFuturisticNav ? <FuturisticNav /> : <Header />}
       <main className="flex-1 w-full">{children}</main>
       <Footer />
       <WhatsAppPopup />
